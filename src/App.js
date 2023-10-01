311dde7aac65843feea9681abae2801fe6ddd65b
Viacheslav <surzhenkoviacheslav@gmail.com>
2023-10-01 21:22:44 +0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import GlobalStyle from "./components/GlobalStyle";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+import { Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Home from "./components/Home";
+
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
-      <h1>Welcome to student portal</h1>
+      <GlobalStyle />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/register" exact element={<Register />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <Navigate to="/login" />
+    </div>
+  );
+};
+
+export default Home;
