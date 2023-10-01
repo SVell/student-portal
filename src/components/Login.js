7653f78a81cfa5fecd0028d37988e1625ca37679
Viacheslav <surzhenkoviacheslav@gmail.com>
2023-10-01 21:52:11 +0300

    Base login and register

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,36 @@
+import React, { useState } from "react";
+
 const Login = () => {
+  const [password, setPassword] = useState("Password");
+  const [visible, setVisible] = useState(true);
+
   return (
     <div>
-      <h1>Login</h1>
+      <div className="login">
+        <h2>Login</h2>
+        <h4>Enter your account details</h4>
+        <form>
+          <div className="username">
+            <input value="Username" type="text" placeholder="Username"></input>
+          </div>
+          <div className="password">
+            <input
+              value={password}
+              type={visible ? "text" : "password"}
+              id="passwordInput"
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+            <button>Eye</button>
+          </div>
+        </form>
+        <p>Forgot Password?</p>
+        <button>Login</button>
+        <div className="sing-up">
+          <p>Don't Have an account?</p>
+          <button>Sign Up</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,49 @@
+import React, { useState } from "react";
+
 const Register = () => {
+  const [password, setPassword] = useState("Password");
+  const [repeatPassword, setRepeatPassword] = useState("Repeat Password");
+  const [visible, setVisible] = useState(true);
+  const [visibleRepeat, setVisibleRepeat] = useState(true);
+
   return (
     <div>
-      <h1>Register</h1>
+      <div className="register">
+        <h2>Register</h2>
+        <h4>Enter your details</h4>
+        <form>
+          <div className="username">
+            <input value="Username" type="text" placeholder="Username"></input>
+          </div>
+          <div className="email">
+            <input value="Email" type="email" placeholder="Email"></input>
+          </div>
+          <div className="password">
+            <input
+              value={password}
+              type={visible ? "text" : "password"}
+              id="passwordInput"
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+            <button>Eye</button>
+          </div>
+          <div className="repeat-password">
+            <input
+              value={repeatPassword}
+              type={visibleRepeat ? "text" : "password"}
+              id="passwordInput"
+              placeholder="password"
+              onChange={(e) => setRepeatPassword(e.target.value)}
+            ></input>
+            <button>Eye</button>
+          </div>
+        </form>
+        <div className="sing-up">
+          <p>Already have an account?</p>
+          <button>Sign In</button>
+        </div>
+      </div>
     </div>
   );
 };
